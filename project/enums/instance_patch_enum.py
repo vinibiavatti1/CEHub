1e94abdc0faf1242e0624975044313bda898ef00
Vinícius Biavatti <vinicius.biavatti@create.pt>
2021-11-03 23:32:50 +0000

    fix

--- project/enums/instance_patch_enum.py
+++ project/enums/instance_patch_enum.py
@@ -5,4 +5,4 @@ class InstancePatchEnum(Enum):
     PATCH_133 = 'Patch v1.33'
     PATCH_141 = 'Patch v1.41'
     PATCH_142 = 'Patch v1.42 (SEB Fix)'
-    PATCH_143 = 'Patch v1.43 (Dafoosa\'s Patch) (Recomended)'
+    PATCH_143 = 'Patch v1.43 (Dafoosa\'s Patch) (Recommended)'
